1651ab5e750466175e750cf20cf5166fbf0744d8
comp <comp@comp-PC>
2018-10-27 13:56:53 +0530

    gfghh

---
+++ fg/src/fg/Login.java
@@ -0,0 +1,19 @@
+package fg;
+import javax.servlet.http.*;  
+import javax.servlet.*;  
+
+import java.io.*; 
+
+@SuppressWarnings("serial")
+public class Login extends HttpServlet{  
+public void doGet(HttpServletRequest req,HttpServletResponse res)  
+throws ServletException,IOException  
+{  
+res.setContentType("text/html");  
+PrintWriter pw=res.getWriter();  
+  
+String name=req.getParameter("name");//will return value  
+pw.println("Welcome "+name);  
+  
+pw.close();  
+}}  
